5a739ef0a72e5c2de529ce66b4ed83e0e5e044d3
sararoseanesm@gmail.com <sararoseanemota>
2024-05-01 12:09:10 -0300

    feat: add initial search component

---
+++ app/_components/search.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+import { SearchIcon } from "lucide-react";
+
+const Search = () => {
+  return (
+    <div className="flex gap-2">
+      <Input placeholder="Buscar restaurantes" className="border-none " />
+      <Button size="icon">
+        <SearchIcon size={20} />
+      </Button>
+    </div>
+  );
+};
+
+export default Search;
